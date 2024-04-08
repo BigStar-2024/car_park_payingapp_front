5e045529a67dcd26b8e6a3eafe15ff6a9cfb55ef
BigStar2024 <138141789+BigStar-2024@users.noreply.github.com>
2024-04-08 20:20:20 +1000

    Payment completion update

--- frontend/src/components/Modal/Completion.js
+++ frontend/src/components/Modal/Completion.js
@@ -23,15 +23,15 @@ function Completion(props) {
     <>
       <div className="fixed top-0 left-0 w-full h-full bg-black bg-opacity-50 flex items-center justify-center">
         <div
-          className="bg-white rounded-lg border-solid border-[5px] border-[#grey] relative py-10 px-20 m-10"
+          className=" rounded-[20px] border-solid border-[0px] bg-[#FFFAF9] relative py-10 px-20 m-10 shadow-lg"
           style={{ transition: "transform 3s", transform: "scale(1)" }}
         >
           <div className='flex flex-col justify-center items-center'>
-            <p className='text-6xl text-[#FA551D] font-bold mt-6'>Thank you!</p>
-            <p className='text-4xl text-[#091C62] font-bold mt-10'>Payment succeeded!</p>
             <div className="mt-5">
               <img src={success} alt="Success" className="success-img"></img>
             </div>
+            <p className='text-4xl text-[#FA551D] font-bold mt-4'>Payment succeeded!</p>
+            <p className='text-2xl text-[#091C62] font-bold mt-6'>Thanks for your park!</p>
             <a href="/" className="text-2xl text-[#091C62] mx-2 underline mt-4">HOME</a>
             <div id="messages" role="alert" style={messageBody ? { display: 'block' } : {}}>{messageBody}</div>
           </div>
